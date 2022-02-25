2ff8a882f6b6a74c73762649bb06c5c8f6a3bdad
Daniel Almachi <ddalmach@pichincha.com>
2022-02-25 09:51:44 -0500

    Initial commit

---
+++ src/main/java/io/javadev/MathUtils.java
@@ -0,0 +1,20 @@
+package io.javadev;
+
+public final class MathUtils {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int substract(int x, int y){
+        return x -y;
+    }
+
+    public float divide(int x, int y) {
+        return x / y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+}
