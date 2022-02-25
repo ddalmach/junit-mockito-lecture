114d84cbe10ddbd3681a2f2d80645ca64e412048
Daniel Almachi <ddalmach@pichincha.com>
2022-02-25 10:11:05 -0500

    using @AfterEach

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,16 +1,24 @@
 import io.javadev.MathUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class MathUtilsTest {
 
     private MathUtils mathUtils;
 
+    @BeforeAll
+    static void firstSetup(){
+        System.out.println("This is execute before the test class initialization ");
+    }
+
     @BeforeEach
     void setup(){
         System.out.println("This is executed before every test");
         mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanUp(){
+        System.out.println("This is executed after every test");
     }
 
     @Test
