8d3fb6a8e442c83101547afe4dbd408f14837477
Daniel Almachi <ddalmach@pichincha.com>
2022-02-25 10:07:52 -0500

    Using @BeforeEach

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,18 +1,25 @@
 import io.javadev.MathUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+    private MathUtils mathUtils;
+
+    @BeforeEach
+    void setup(){
+        System.out.println("This is executed before every test");
+        mathUtils = new MathUtils();
+    }
+
     @Test
     void shouldReturnTheResultTwoWhenAddingTwoNumbers(){
-        MathUtils mathUtils = new MathUtils();
         Assertions.assertEquals(2, mathUtils.add(1,1));
     }
 
     @Test
     void shouldReturnTheResultThreeWhenSubstractingTwoNumbers(){
-        MathUtils mathUtils = new MathUtils();
         Assertions.assertEquals(3, mathUtils.substract(6,3));
     }
 }
